bfc9ffa5942e3fc0f6738f2d97afcb02fdc80e0b
heri wahyudianto <heriwahyudianto@gmail.com>
2019-04-26 10:38:51 +0700

    delete cache

--- sw.js
+++ sw.js
@@ -24,10 +24,32 @@ self.addEventListener('install', e => {
     })
   );
 });
-
+/*
 self.addEventListener('activate', event => {
   event.waitUntil(self.clients.claim());
+}); */
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.delete(cacheName).then(function(boolean) {
+    console.log('your cache is now deleted');
+  }));
 });
+
+/*
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.filter(function(cacheName) {
+          // Return true if you want to remove this cache,
+          // but remember that caches are shared across
+          // the whole origin
+        }).map(function(cacheName) {
+          return caches.delete(cacheName);
+        })
+      );
+    })
+  );
+}); */
 
 self.addEventListener('fetch', event => {
   event.respondWith(
